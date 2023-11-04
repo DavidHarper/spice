b9b06f6069c059ef7d4d00e1a0e7650992a4381e
David Harper <david@obliquity.net>
2023-11-04 13:45:08 +0000

    Add magnirude and apparent diameter plots.

--- coorbital-earths.py
+++ coorbital-earths.py
@@ -5,7 +5,7 @@
 #
 # It also uses the SpiceyPy wrapper for NAIF SPICE
 
-from math import sqrt, pi, sin, cos, atan2, fabs
+from math import sqrt, pi, sin, cos, atan2, fabs, log10, asin
 import sys
 import glob
 import spiceypy as spice
@@ -70,6 +70,15 @@ xEarth2Earth1 = []
 yEarth2Earth1 = []
 qEarth2Earth1 = []
 elEarth2 = []
+magEarth2 = []
+diaEarth2 = []
+
+LOG_AU = log10(149600000.0)
+
+def magnitude(r, d, p):
+    theta = p/100.0
+    dm = 5.0 * (log10(r*d) - 2.0 * LOG_AU)
+    return -4.40 + dm + 0.09 * theta + 2.39 * theta * theta - 0.65 * theta * theta * theta
 
 def heartbeat(sim_pointer):
     sim = sim_pointer.contents
@@ -91,10 +100,13 @@ def heartbeat(sim_pointer):
     tdata.append(tjd/365.25)
 
     rEarth1.append(spice.vnorm(pEarth1) - aEarth1)
-    rEarth2.append(spice.vnorm(pEarth2) - aEarth2)
+
+    r2 = spice.vnorm(pEarth2)
+    rEarth2.append(r2 - aEarth2)
 
     pEarth2Earth1 = spice.vsub(pEarth2, pEarth1)
-    rEarth2Earth1.append(spice.vnorm(pEarth2Earth1))
+    delta = spice.vnorm(pEarth2Earth1)
+    rEarth2Earth1.append(delta)
 
     xEarth2Earth1.append(spice.vdot(u, pEarth2Earth1))
     yEarth2Earth1.append(spice.vdot(v, pEarth2Earth1))
@@ -103,8 +115,15 @@ def heartbeat(sim_pointer):
     dy = spice.vdot(v, pEarth2)
     qEarth2 = 180.0*atan2(dy, dx)/pi
     qEarth2Earth1.append(qEarth2)
+    phaseEarth2 = 90.0-fabs(0.5*qEarth2)
 
-    elEarth2.append(90.0-fabs(0.5*qEarth2))
+    elEarth2.append(phaseEarth2)
+
+    mag = magnitude(r2, delta, phaseEarth2)
+    magEarth2.append(mag)
+
+    dia = 2.0 * asin(6378.0/delta) * 3600.0 * 180.0/pi
+    diaEarth2.append(dia)
 
 sim.heartbeat=heartbeat
 sim.t = 0.0
@@ -125,7 +144,7 @@ ns_per_step = ns_run/steps
 print(f"Ran {steps} steps in {ns_run:,} ns which is {ns_per_step:0f} ns per step")
 
 fig = plt.figure(tight_layout=True)
-gs = gridspec.GridSpec(5, 2)
+gs = gridspec.GridSpec(7, 2)
 
 axDistances = fig.add_subplot(gs[0, :])
 axDistances.plot(tdata, rEarth1, label='r[Earth1]')
@@ -144,7 +163,15 @@ axElong = fig.add_subplot(gs[3, :])
 axElong.plot(tdata, elEarth2, label='el[Earth2]')
 axElong.legend()
 
-axEarth1Frame = fig.add_subplot(gs[4, 0])
+axMag = fig.add_subplot(gs[4, :])
+axMag.plot(tdata, magEarth2, label='mag[Earth2]')
+axMag.legend()
+
+axDia = fig.add_subplot(gs[5, :])
+axDia.plot(tdata, diaEarth2, label='dia[Earth2]')
+axDia.legend()
+
+axEarth1Frame = fig.add_subplot(gs[6, 0])
 axEarth1Frame.plot([0], [0], "or")
 axEarth1Frame.plot(xEarth2Earth1, yEarth2Earth1, label='Earth2')
 axEarth1Frame.set(aspect=1)
