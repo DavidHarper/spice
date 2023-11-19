82b3cd1ce48e810b535aba1f8c2541324076bc58
David Harper <david@obliquity.net>
2023-11-19 08:49:07 +0000

    Further tinkering with Moon intercept program.

--- apollo.py
+++ apollo.py
@@ -58,16 +58,22 @@ moon = rebound.Particle(m=mMoon, x=aMoon, y=0.0, z=0.0, vx=0.0, vy=vMoon, vz=0.0
 
 sim.add(moon)
 
-aApollo = aeEarth + altitude
+aApollo0 = aeEarth + altitude
+nApollo0 = sqrt(gmEarth/aApollo0**3)
+vApollo0 = nApollo0 * aApollo0
 
-nApollo = sqrt(gmEarth/aApollo**3)
-vApollo = nApollo * aApollo + deltav
+x0Apollo = aApollo0 * cos(phase)
+y0Apollo = aApollo0 * sin(phase)
 
-x0Apollo = aApollo * cos(phase)
-y0Apollo = aApollo * sin(phase)
+aApollo1 = 0.5 * ( aeEarth + altitude + aMoon )
+vApollo1 = sqrt(gmEarth * (2.0/aApollo0 - 1.0/aApollo1))
 
-vx0Apollo = - vApollo * sin(phase)
-vy0Apollo =   vApollo * cos(phase)
+print("At altitude %.2f km, circular velocity is %.3f km/s, Hohmann transfer velocity is %.3f km/s" % (altitude, vApollo0, vApollo1))
+
+vApollo1 = vApollo1 + deltav
+
+vx0Apollo = - vApollo1 * sin(phase)
+vy0Apollo =   vApollo1 * cos(phase)
 
 apollo = rebound.Particle(m=0.0, x=x0Apollo, y=y0Apollo, z=0.0, vx=vx0Apollo, vy=vy0Apollo, vz=0.0)
 
@@ -75,8 +81,12 @@ sim.add(apollo)
 
 tdata = []
 
-xApollo = []
-yApollo = []
+xApolloRotating = []
+yApolloRotating = []
+xApolloFixed = []
+yApolloFixed = []
+xApolloMoon = []
+yApolloMoon = []
 
 def heartbeat(sim_pointer):
     sim = sim_pointer.contents
@@ -94,18 +104,21 @@ def heartbeat(sim_pointer):
     v = spice.vhat(spice.vcrss(w, u))
 
     pApollo= np.array([apollo.x - earth.x, apollo.y - earth.y, apollo.z - earth.z], dtype = float)
+    pApolloMoon = np.array([apollo.x - moon.x, apollo.y - moon.y, apollo.z - moon.z], dtype = float)
 
     tdata.append(tjd)
+
+    xApolloFixed.append(apollo.x - earth.x)
+    yApolloFixed.append(apollo.y - earth.y)
 
     xa = spice.vdot(u, pApollo)
     ya = spice.vdot(v, pApollo)
 
-    xApollo.append(xa)
-    yApollo.append(ya)
+    xApolloRotating.append(xa)
+    yApolloRotating.append(ya)
 
-    ra = sqrt(xa*xa+ya*ya)
-
-    #print("%10.4f  %10.2f %10.2f  %10.2f" % (tjd, xa, ya, ra))
+    xApolloMoon.append(spice.vdot(u, pApolloMoon))
+    yApolloMoon.append(spice.vdot(v, pApolloMoon))
 
 sim.heartbeat=heartbeat
 sim.t = 0.0
@@ -126,12 +139,21 @@ ns_per_step = ns_run/steps
 print(f"Ran {steps} steps in {ns_run:,} ns which is {ns_per_step:0f} ns per step")
 
 fig = plt.figure(tight_layout=True)
-gs = gridspec.GridSpec(1, 1)
+gs = gridspec.GridSpec(3, 1)
 
-axApollo = fig.add_subplot(gs[0, 0])
-axApollo.plot([0], [0], "or")
-axApollo.plot([aMoon], [0], "og")
-axApollo.plot(xApollo, yApollo, label='Earth2')
-axApollo.set(aspect=1)
+axApollo1 = fig.add_subplot(gs[0, 0])
+axApollo1.plot([0], [0], "or")
+axApollo1.plot(xApolloFixed, yApolloFixed, label='Earth centred, Fixed frame')
+axApollo1.set(aspect=1)
+
+axApollo2 = fig.add_subplot(gs[1, 0])
+axApollo2.plot([0], [0], "or")
+axApollo2.plot(xApolloRotating, yApolloRotating, label='Earth centred, Rotating frame')
+axApollo2.set(aspect=1)
+
+axApollo3 = fig.add_subplot(gs[2, 0])
+axApollo3.plot([0], [0], "og")
+axApollo3.plot(xApolloMoon, yApolloMoon, label='Moon centred, rotating frame')
+axApollo3.set(aspect=1)
 
 plt.show()
